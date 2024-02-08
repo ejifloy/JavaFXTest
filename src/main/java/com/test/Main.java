a36c1ea186693a2ab07a46bf91fafde6330031e0
ejifloy <e.g.1992@live.de>
2024-02-08 21:15:25 +0100

    First commit JavaFXTest 08.02.2024 21:14

---
+++ src/main/java/com/test/Main.java
@@ -0,0 +1,23 @@
+package com.test;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
+    public static void main(String[] args) {
+        launch();
+    }
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/fxml/test.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Hello Test");
+        stage.setScene(scene);
+        stage.show();
+    }
+}
---
+++ src/main/java/com/test/MainController.java
@@ -0,0 +1,49 @@
+package com.test;
+
+import com.jfoenix.controls.JFXTextArea;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainController implements Initializable {
+    @FXML
+    private JFXTextArea fxTextArea;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle){
+
+    }
+
+    @FXML
+    private void setCheck(){
+
+    }
+
+    @FXML
+    protected void setText() {
+
+//        try{
+//            Class.forName("com.mysql.cj.jdbc.Driver");
+//            Connection connection = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/userdb","root","pw123456");
+//
+//            Statement statement = connection.createStatement();
+//            String sql = "SELECT * FROM userdb.users;";
+//            ResultSet resultSet = statement.executeQuery(sql);
+//
+//            while(resultSet.next()){
+//                fxTextArea.appendText(resultSet.getString("user_name")
+//                        +"\t"+resultSet.getString("user_password")
+//                        +"\t"+resultSet.getBoolean("skin")
+//                        +"\t"+resultSet.getBoolean("remember")+"\n");
+//            }
+//
+//            connection.close();
+//        }catch (Exception e){
+//            System.out.println("Error: "+e);
+//        }
+    }
+
+
+}
