f31992e5e652899c7d055ac578467703c8b2827d
ejifloy <e.g.1992@live.de>
2024-02-09 13:36:03 +0100

    First Change on AddJFoenix-Branch

--- src/main/java/com/test/Main.java
+++ src/main/java/com/test/Main.java
@@ -19,5 +19,7 @@ public class Main extends Application {
         stage.setTitle("Hello Test");
         stage.setScene(scene);
         stage.show();
+
+        //TODO: Add JFoenix Libaries for deployment...
     }
 }
